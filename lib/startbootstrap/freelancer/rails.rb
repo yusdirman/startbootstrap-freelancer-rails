1a173ea83f2d9308ed7af5adb1ac32d7a1a97960
yusdirman <yusdirman@gmail.com>
2017-09-27 00:19:50 +0800

    init

---
+++ lib/startbootstrap/freelancer/rails.rb
@@ -0,0 +1,9 @@
+require "startbootstrap/freelancer/rails/version"
+
+module Startbootstrap
+  module Freelancer
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/startbootstrap/freelancer/rails/version.rb
@@ -0,0 +1,7 @@
+module Startbootstrap
+  module Freelancer
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
